5350a9fae404887c0724cf027b88f4d696d8a255
kusuma231 <kusumabrkusumabr44@gmail.com>
2025-12-11 15:51:36 +0530

    Add files via upload

---
+++ day2/Merge.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class Merge {
+	public static void main (String[] args)
+	{
+		int[] arr1 = {1, 2, 3, 4};
+		int[] arr2 = {8, 7, 6, 9};
+		int[] merged = new int[arr1.length + arr2.length];
+		for(int i=0; i<arr1.length; i++)
+		{
+			merged[i] = arr1[i];
+		}
+		for(int i=0; i<arr2.length; i++)
+		{
+			merged[arr1.length + i]	= arr2[i];
+		}
+		System.out.println("Merged  Array:");
+		for(int i=0; i<merged.length; i++)
+		{
+			System.out.println(merged[i] + " ");
+		}
+	}
+}
