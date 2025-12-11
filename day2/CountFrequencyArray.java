b044e6ba199687d4cec274e8cd9ea68509cb7fe0
kusuma231 <kusumabrkusumabr44@gmail.com>
2025-12-11 14:59:24 +0530

    Add files via upload

---
+++ day2/CountFrequencyArray.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class CountFrequencyArray {
+	public static void main(String[] args) 
+	{
+		int[ ] arr= {1, 2, 3, 4, 5, 6, 1, 2, 4, 2};
+		boolean[] visited = new boolean[arr.length];
+		for(int i=0; i< arr.length; i++)
+		{
+			if (visited[i])
+			{
+				continue;
+			}
+			int count=1;
+			for (int j =i; j < arr.length; j++)
+			{
+				if (arr[i]==arr[j])
+				{
+					visited[j]=true;
+					count++;
+				}
+			}
+			System.out.println(arr[i]+"->"+count);
+		}
+	}
+}
