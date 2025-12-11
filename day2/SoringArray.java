2fe37f7716d3189bdc671bd37cc74517f97a275b
kusuma231 <kusumabrkusumabr44@gmail.com>
2025-12-11 14:32:42 +0530

    Add files via upload

---
+++ day2/SoringArray.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class SoringArray {
+	public static void main(String[] args) {
+	int[ ] arr= {1,2,3,4};
+	
+	for(int i=0; i<arr.length-1; i++)
+	{
+		for(int j=0; j<arr.length-1; j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	for(int num:arr)
+	{
+		System.out.println(num+" ");
+	}
+}
+}
