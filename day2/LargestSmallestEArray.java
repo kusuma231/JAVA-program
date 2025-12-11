b8255d69b95463d0203d9a6e00707616def0756e
kusuma231 <kusumabrkusumabr44@gmail.com>
2025-12-11 12:25:01 +0530

    Add files via upload

---
+++ day2/LargestSmallestEArray.java
@@ -0,0 +1,23 @@
+package kusuma;
+
+public class LargestSmallestEArray {
+public static void main(String[] args)
+{
+	int[ ] numbers= {10,20,30,40,2,4,21,9};
+	int smallest=numbers[0];
+	int largest=numbers[0];
+	for(int i=1; i<numbers.length; i++)
+	{
+		if(numbers[i]<smallest) 
+		{
+		smallest=numbers[i];
+		}
+		if(numbers[i]>largest)
+		{
+			largest=numbers[i];	
+	}
+	}
+	System.out.println("smallest+number:"+smallest);
+	System.out.println("largest+number:"+largest);
+}
+}
