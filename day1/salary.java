13aee8c941403c30c2d17649ec1055d89ece70eb
kusuma231 <kusumabrkusumabr44@gmail.com>
2025-12-11 12:06:56 +0530

    Update salary.java

--- day1/salary.java
+++ day1/salary.java
@@ -1,5 +1,19 @@
 package kusuma;
 
-public class salary {
-
+public class salary
+{
+double finalsalary;
+public void increment()
+{
+	double salary=500000.0;
+	double bonusPercentage=10.0;
+	finalsalary= salary+(salary*bonusPercentage/100);
 }
+public static void main(String[] args)
+{
+	salary salary=new salary();
+	salary.increment();
+	System.out.println("salary increment amout: "+salary.finalsalary);
+}
+}
+
