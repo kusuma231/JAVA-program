dd7a46e8a751d065353fdb3aefcfff7ded1780ae
kusuma231 <kusumabrkusumabr44@gmail.com>
2025-12-18 11:00:17 +0530

    Create RemoveSpaces.java

---
+++ day3/RemoveSpaces.java
@@ -0,0 +1,9 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String s = "Good Morning";
+        System.out.println(s.replace(" ", ""));
+    }
+}
+/*
+GoodMorning
+*/
