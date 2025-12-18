94654d58eb491839bcccdf1d64dde5b1016a3f0c
kusuma231 <kusumabrkusumabr44@gmail.com>
2025-12-18 11:11:25 +0530

    Create CountWords.java

---
+++ day3/CountWords.java
@@ -0,0 +1,10 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String s = "I love Java programming";
+        String[] words = s.split(" ");
+        System.out.println("Word count: " + words.length);
+    }
+}
+/*
+Word count: 4
+*/
