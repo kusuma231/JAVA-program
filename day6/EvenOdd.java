4a15c8a71888f6aa9a9d1adfc7336012f25c5168
kusuma231 <kusumabrkusumabr44@gmail.com>
2025-12-18 12:21:01 +0530

    Create EvenOdd.java

---
+++ day6/EvenOdd.java
@@ -0,0 +1,38 @@
+class EvenThread extends Thread {
+    public void run() {
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println("Even: " + i);
+        }
+    }
+}
+
+class OddThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.println("Odd: " + i);
+        }
+    }
+}
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        EvenThread e = new EvenThread();
+        OddThread o = new OddThread();
+
+        e.start();
+        o.start();
+    }
+}
+/*
+Odd: 1
+Even: 2
+Odd: 3
+Even: 4
+Odd: 5
+Even: 6
+Odd: 7
+Even: 8
+Odd: 9
+Even: 10
+
+*/
